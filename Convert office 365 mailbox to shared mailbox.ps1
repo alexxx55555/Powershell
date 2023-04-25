2cf5f5580c2730192ef25d80115ed04331651236
alexxx55555 <56542042+alexxx55555@users.noreply.github.com>
2023-04-25 19:11:18 +0300

    Add files via upload

---
+++ Convert office 365 mailbox to shared mailbox.ps1
@@ -0,0 +1,10 @@
+﻿# Set the user mailbox you want to convert to a shared mailbox
+$userMailbox = "user@example.com"
+
+# Connect to Exchange Online PowerShell
+Connect-ExchangeOnline
+
+# Convert the user mailbox to a shared mailbox
+Set-Mailbox -Identity $userMailbox -Type Shared
+
+Write-Host "User mailbox $($userMailbox) has been converted to a shared mailbox."
